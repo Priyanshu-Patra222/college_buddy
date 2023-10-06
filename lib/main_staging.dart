fa9f09a287012abc815d1f89c9b557a878fe8a50
Priyanshu Patra <patra.priyanshu02@gmail.com>
2023-10-06 13:24:54 +0530

    ✨ Initial commit

---
+++ lib/main_staging.dart
@@ -0,0 +1,8 @@
+import 'package:college_buddy/app/app.dart';
+import 'package:college_buddy/bootstrap.dart';
+
+/// This entry point should be used for staging only
+void main() {
+  ///You can override your environment variable in bootstrap method here for providers
+  bootstrap(() => const App());
+}
---
+++ riverpod_simple_architecture.md
@@ -0,0 +1,2 @@
+Hello college_buddy!
+Made using riverpod_simple_architecture.
