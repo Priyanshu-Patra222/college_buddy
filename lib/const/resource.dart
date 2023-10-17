cd62a3ddcc2216fb6d1d7cd60fafb2d32ff342cc
ferajofficial <officialferaj@gmail.com>
2023-10-17 21:05:08 +0530

    HomeUI

--- lib/const/resource.dart
+++ lib/const/resource.dart
@@ -4,6 +4,19 @@
 class R {
   const R._();
 
-  /// ![preview](file://C:\Users\shree\Documents\GitHub\college_buddy\assets\anim\nointernet.riv)
-  static const String ASSETS_ANIM_NOINTERNET_RIV = 'assets/anim/nointernet.riv';
+  /// ![preview](file:///Users/skferajuddin/Desktop/development/FlutterProjects/college_buddy/assets/IMG_20230928_183603.jpg)
+  static const String ASSETS_IMG_20230928_183603_JPG =
+      'assets/IMG_20230928_183603.jpg';
+
+  /// ![preview](file:///Users/skferajuddin/Desktop/development/FlutterProjects/college_buddy/assets/icons/bank.svg)
+  static const String ASSETS_ICONS_BANK_SVG = 'assets/icons/bank.svg';
+
+  /// ![preview](file:///Users/skferajuddin/Desktop/development/FlutterProjects/college_buddy/assets/icons/home.svg)
+  static const String ASSETS_ICONS_HOME_SVG = 'assets/icons/home.svg';
+
+  /// ![preview](file:///Users/skferajuddin/Desktop/development/FlutterProjects/college_buddy/assets/icons/notice.svg)
+  static const String ASSETS_ICONS_NOTICE_SVG = 'assets/icons/notice.svg';
+
+  /// ![preview](file:///Users/skferajuddin/Desktop/development/FlutterProjects/college_buddy/assets/mars.jpg)
+  static const String ASSETS_MARS_JPG = 'assets/mars.jpg';
 }
---
+++ lib/features/noticepage/notice_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NoticePage extends StatelessWidget {
+  const NoticePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Notice Page'),
+      )
+    );
+  }
+}
